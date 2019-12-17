00ac72ae233b67c8d10e80f65fe1d463f5feec38
manoj <mkmittal2018@gmail.com>
2019-12-17 18:49:03 +1100

    terraform File

---
+++ ec2/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "ap-southeast-2"
+  access_key = "AKIA6F76RJ6TIOLFIDLS"
+}
+
+resource "aws_instance" "ec2_instance" {
+  ami = "ami-0975ce566eec139c3"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0d57354edfc68dd18"]
+  key_name = "myec2"
+  tags = {
+    Name = "example instance"
+  }
+
+}
